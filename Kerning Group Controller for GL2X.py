c05b28b3292d9fe22ae82ac44ad60089636a914a
Alexandr Hudeček <lexislav@centrum.cz>
2019-12-12 10:59:42 +0100

    mistake

--- Kerning Group Controller for GL2X.py
+++ Kerning Group Controller for GL2X.py
@@ -35,19 +35,8 @@ def refreshGlobals():
     global groupsL
     global groupsR
     global kernDic
-    groupsL = {}
-    groupsR = {}
     kernDic = thisFont.kerningDict()
-    for thisGlyph in thisFont.glyphs:
-        if thisGlyph.rightKerningGroup != None:
-    		if not thisGlyph.rightKerningGroup in groupsL:
-    			groupsL[thisGlyph.rightKerningGroup] = []
-    		groupsL[thisGlyph.rightKerningGroup].append(thisGlyph.name)
-        if thisGlyph.leftKerningGroup != None:
-            if not thisGlyph.leftKerningGroup in groupsR:
-    			groupsR[thisGlyph.leftKerningGroup] = []
-            groupsR[thisGlyph.leftKerningGroup].append(thisGlyph.name)
-    return True
+    return
 
 refreshGlobals()
 
@@ -77,9 +66,6 @@ class AppController:
     def __init__(self):
         if not run:
             return
-        if groupsL:
-            self.selectedGroupName = sorted(groupsL)[0]
-            self.refreshSelectedGroupGlyphs(groupsL)
         self.w = self.getWindow()
         self.w.open()
         pass
